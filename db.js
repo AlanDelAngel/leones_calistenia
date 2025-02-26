db1e626d86e6f1ab1e99d9f5ca4e2cdc97b086e5
Adelfo <adelfodangf@gmail.com>
2025-02-26 13:20:11 -0600

    added notes to change between local testing and gcloudusage for the db

--- db.js
+++ db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2/promise'); // Use the promise version
 require('dotenv').config();
 
 const pool = mysql.createPool({
+    socketPath: `/cloudsql/${process.env.DB_HOST}`, // to switch between gcloud db and localhost see .env notes
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    socketPath: `/cloudsql/${process.env.DB_HOST}`, // Cloud SQL Connection
+    port: process.env.DB_PORT,
     connectionLimit: 50
   });
 
